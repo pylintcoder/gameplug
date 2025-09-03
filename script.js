d75936e40e1d17196b6797e8206ae1f1d9c7b5b4
pylintcoder <148223068+pylintcoder@users.noreply.github.com>
2025-09-03 01:01:14 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const navList = document.querySelector('.firstli')
+const menu = document.querySelector('.bx');
+
+menu.addEventListener('click', ()=> {
+    navList.classList.add('active-popup'); 
+});
