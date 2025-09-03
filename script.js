89e346650e04d3d84fe5ed5663ed577819401c3f
pylintcoder <148223068+pylintcoder@users.noreply.github.com>
2025-09-03 00:59:53 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const navList = document.querySelector('.firstli')
-const menu = document.querySelector('.bx');
-
-menu.addEventListener('click', ()=> {
-    navList.classList.add('active-popup'); 
-});
